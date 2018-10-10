ffd02b276cd9ad97fab70e78846e05a7cf87e7fd
wangshiyi <wangshiyi@minipo.com>
2018-10-10 12:49:14 +0800

    功能完善

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,11 @@
 const config = {
 	"app_key": "25r94v1satpdv50p",
 	"app_secret": "31wgjqzicf4ty5pl429vkrolosi7inyf",
-	"server": "http://api.wanghei.tech",
+	"server": "https://api.wanghei.tech",
 	"img_server": 'http://admin.wanghei.tech'
-  }
-  
+}
+
 export {
 	config
 };
-  
+
